5f49acc50e05d1f31043bfe25dce6ffc36b9eed6
18smi <18smith.ben@gmail.com>
2026-04-11 20:45:57 +0000

    a fue missing parts left

--- python/Bens_stuff.py
+++ python/Bens_stuff.py
@@ -1,74 +1,78 @@
-import csv
+import pandas as pandas
+
+CSV_NAME = "five_year_dataset.csv"
 
 DEFALT_YEAR = 0
 DEFALT_QUATER = 0
-STARTING_REGION = "blank"
 DEFALT_REGION = "United Kingdom"
+STARTING_REGION = "blank"
 
-def main():
-    with open('five_year_dataset.csv', mode='r') as file:
-        year = DEFALT_YEAR
-        quater = DEFALT_QUATER
-        region = STARTING_REGION
+def main():    
+    year = DEFALT_YEAR
+    quater = DEFALT_QUATER
+    region = STARTING_REGION
 
-        print("opening sqrall 'skip' skips the field")
-
-        
-        while True:# date
-            date = input("input date form 1/1/2021 to 31/12/2025 (optional)")
-            if date == "skip":
-                break
-
-            if not valid_date(date):
-                print("invalid date")
-                continue
-            
-            year = year_finder(date)
-            quater = quater_finder(date)
-            
-
-        while True:# region
-            input_region = input("input region (optional)")
-            if input_region == "skip":
-                break
-
-            if not valid_region(input_region): #invalid region
-                print("invalid date")
-                continue
-            
-            region = input_region
+    print("how to use guide 'skip' skips the field")
 
         
-        while True:# region code
-            region_code = input("enter region code (optional)")
-            if region_code == "skip":
-                break
+    while True:# date
+        date = input("input date form 1/1/2021 to 31/12/2025 (optional):  ")
+        if date.lower() == "skip":
+            break
+
+        if not valid_date(date):
+            print("invalid date")
+            continue
             
-            if not valid_region_code:
-                print("invalid region code")
-                continue
-
-            if region != region_code_to_region(region_code) and region != "blank":
-                print("region vs region code conflict")
-                continue
+        year = year_finder(date)
+        quater = quater_finder(date)
+        break
             
-            region = region_code_to_region(region_code)
 
-        if region == STARTING_REGION:
-            region = DEFALT_REGION
+    while True:# region
+        input_region = input("input region (optional):  ")
+        if input_region.lower() == "skip":
+            break
 
+        if not valid_region(input_region): #invalid region
+            print("invalid date")
+            continue
+            
+        region = input_region
+        break
 
-        print("region, year, quater")
-        print(region, year, quater)
+        
+    while True:# region code
+        region_code = input("enter region code (optional):  ")
+        if region_code.lower() == "skip":
+            break
+            
+        if not valid_region_code:
+            print("invalid region code")
+            continue
 
-        # defalt date = avrage over all
+        if region != region_code_to_region(region_code) and region != "blank":
+            print("region vs region code conflict")
+            continue
+            
+        region = region_code_to_region(region_code)
+        break
+
+    if region == STARTING_REGION:
+        region = DEFALT_REGION
+
+    print('\n')
+
+    display_prices(region, year, quater)
+    
+
+        
         # output expected New dwellings Price,New dwellings average advance,New dwellings average recorded income of borrowers,Other dwellings Price,Other dwellings average advance,Other dwellings average recorded income of borrowers,All dwellings Price,All dwellings average advance,All dwellings average recorded income of borrowers,First time buyers Price,First time buyers average advance,First time buyers average recorded income of borrowers,Former owner occupiers Price,Former owner occupiers average advance,Former owner occupiers average recorded income of borrowers
 
 
 
-
 def valid_date(date):
-    date.split('/')
+    date = date.split('/')
     if len(date) != 3:
         return False
     if 2021 > int(date[2]) or 2025 < int(date[2]):
@@ -104,6 +108,62 @@ def region_code_to_region(region_code):
     valid_region_codes = ["K02000001", "K03000001", "K04000001", "E92000001", "E12000001", "E12000002", "E12000003", "E12000004", "E12000005", "E12000006", "E12000007", "E12000008", "E12000009", "W92000004", "S92000003", "N92000001"] 
 
     return valid_regions[valid_region_codes.index("region_code")]
+
+def display_prices(region, year, quater):
+    try:# checks all inputs are in the corect type
+        region = str(region)
+        year = int(year)
+        quater = int(quater)
+    except (ValueError, TypeError):
+        return
+    
+    if not 0 < quater < 5:
+        print("invalid quater")
+        return
+    if not 2020 < year < 2026:
+        print("invalid year")
+        return
+    if not valid_region(region):
+        print("invalid region")
+        return
+
+    date = str(year) + " Q" + str(quater)
+
+    csv = pandas.read_csv(CSV_NAME)
+    chosen_row = []
+    if year == DEFALT_YEAR:
+        ...
+        # prices = average
+    else:
+        chosen_row = csv[(csv["Period"] == date) & (csv["Region"] == region)]
+
+    if chosen_row.empty:
+        print("no rows found")
+        return
+
+    
+    print("New dwellings Price = £",(chosen_row.iloc[0])["New dwellings Price"])
+    print("New dwellings average advance = £",(chosen_row.iloc[0])["New dwellings average advance"])
+    print("New dwellings average recorded income of borrowers = £",(chosen_row.iloc[0])["New dwellings average recorded income of borrowers"])
+    print('')
+    print("Other dwellings Price = £",(chosen_row.iloc[0])["Other dwellings Price"])
+    print("Other dwellings average advance = £",(chosen_row.iloc[0])["Other dwellings average advance"])
+    print("Other dwellings average recorded income of borrowers = £",(chosen_row.iloc[0])["Other dwellings average recorded income of borrowers"])
+    print('')
+    print("All dwellings Price = £",(chosen_row.iloc[0])["All dwellings Price"])
+    print("All dwellings average advance = £",(chosen_row.iloc[0])["All dwellings average advance"])
+    print("All dwellings average recorded income of borrowers = £",(chosen_row.iloc[0])["All dwellings average recorded income of borrowers"])
+    print('')
+    print("First time buyers Price = £",(chosen_row.iloc[0])["First time buyers Price"])
+    print("First time buyers average advance = £",(chosen_row.iloc[0])["First time buyers average advance"])
+    print("First time buyers average recorded income of borrowers = £",(chosen_row.iloc[0])["First time buyers average recorded income of borrowers"])
+    print('')
+    print("Former owner occupiers Price = £",(chosen_row.iloc[0])["Former owner occupiers Price"])
+    print("Former owner occupiers average advance = £",(chosen_row.iloc[0])["Former owner occupiers average advance"])
+    print("Former owner occupiers average recorded income of borrowers = £",(chosen_row.iloc[0])["Former owner occupiers average recorded income of borrowers"])
+
+
+    
 
 
 
